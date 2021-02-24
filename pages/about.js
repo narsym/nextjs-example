47975d99c955c3c450a372b4b9290aeee5a5f1e7
Narasimha Vennelaganti <narasimha.00076@gmail.com>
2021-02-24 11:35:46 +0530

    initial

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+const About = () => {
+    return (
+        <>
+            <Head>
+            <title> Ninja List | Home</title>
+            <meta name = "keywords" content = "ninjas"/>
+            </Head>
+            <div>
+                <h1> About </h1>
+                <p> this is a paragraph</p>
+                <p> this is a paragraph</p>
+            </div>
+        </>
+    );
+}
+
+export default About;
---
+++ pages/ninja/test.js
@@ -0,0 +1,10 @@
+const Test = () => {
+    return (
+        <div>
+            <h1> Test </h1>
+            <p> this is a paragraph</p>
+        </div>
+    )
+}
+
+export default Test;
